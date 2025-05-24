7143213b6b952e4fc5742caa481b37a73f8387aa
Ondřej Caha <cahaondra@gmail.com>
2025-05-24 09:56:13 +0200

    Merged main and run

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-mod app;
-mod run;
+use app::App;
+use winit::event_loop::{ControlFlow, EventLoop};
 
-use run::*;
+mod app;
+
+pub async fn run() {
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+    let mut app = App::new();
+    event_loop.run_app(&mut app).unwrap();
+}
 
 fn main() {
     pollster::block_on(run());
